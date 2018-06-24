b522be5207df4f8a7d0891ab262e8c38340f0f18
lianshangyangguang <1141179268@qq.com>
2018-06-24 13:58:18 +0800

    上传代码

---
+++ README.md
@@ -0,0 +1,92 @@
+# WebViewDialog [![License](https://img.shields.io/badge/license-Apache%202-green.svg)](https://www.apache.org/licenses/LICENSE-2.0) [![Download](https://api.bintray.com/packages/huangdali/jwkj/WebViewDialog/images/download.svg) ](https://bintray.com/huangdali/jwkj/WebViewDialog/_latestVersion)
+
+- 显示webview的dialog
+- 背景透明
+
+## 效果图
+
+![](https://github.com/huangdali/WebViewDialog/blob/master/screenshot.png)
+
+
+## 导入
+app.build 导入依赖
+```java
+    compile 'com.jwkj:WebViewDialog:v1.0.6'
+    compile 'com.jwkj:commwebview:v1.1.4'
+```
+
+## 配置混淆
+```java
+#WebViewDialog库
+-keep class com.hdl.webviewdialog.** {  *;}
+-dontwarn com.hdl.webviewdialog.**
+
+#commwebview库
+-keep class com.jwkj.** {  *;}
+-dontwarn com.jwkj.**
+```
+
+## 使用
+
+常规使用：
+
+```java
+        WebViewDialog dialog = new WebViewDialog(this);
+        dialog.loadUrl(url);
+        dialog.show();
+```
+
+如果弹窗是由系统自动完成，而不是用户点击，建议在页面加载完成的时候再弹出：
+```java
+        url = etUrl.getText().toString().trim();
+        final WebViewDialog dialog = new WebViewDialog(this);
+        dialog.loadUrl(url);
+        dialog.getWebView().startCallback(new WebViewCallback() {
+            @Override
+            public void onStart() {
+            }
+
+            @Override
+            public void onProgress(int curProgress) {
+                if (curProgress == 100) {//加载完成的时候再显示
+                    dialog.show();
+                }
+            }
+
+            @Override
+            public void onError(int errorCode, String description, String failingUrl) {
+            }
+        });
+```
+
+
+## h5关闭对话框
+
+需要h5页面编写js代码关闭对话框：
+- 对象名：JsCallNative
+- 方法名：closeDialog
+
+**eg:**
+在需要关闭对话框的地方加入以下代码
+```java
+   onClick="window.JsCallNative.closeDialog()"
+```
+
+
+
+## 版本记录
+
+v1.0.6 ( [2017.08.25]() )
+- 【新增】设置是否显示dialog的出现动画
+
+v1.0.5 ( [2017.08.21]() )
+- 【新增】getWebView方法，可自己扩展配置webview
+
+v1.0.4 ( [2017.08.21]() )
+- 【新增】addJavascriptInterface方法，可自己扩展与Js的交互
+
+v1.0.3 ( [2017.08.21]() )
+- 【优化】删除一些无用资源和日志
+
+v1.0.2 ( [2017.08.21]() )
+- 【新增】背景透明功能
---
+++ webviewdialog_lib/src/main/java/com/hdl/webviewdialog/WebViewDialog.java
@@ -0,0 +1,187 @@
+package com.hdl.webviewdialog;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.annotation.NonNull;
+import android.support.annotation.StyleRes;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.JavascriptInterface;
+
+import com.jwkj.CommWebView;
+
+/**
+ * 弹窗版webview
+ * Created by HDL on 2017/8/18.
+ */
+
+public class WebViewDialog extends Dialog {
+    private Context mContext;
+    private CommWebView webView;
+    private int screenWidth, screenHeight;
+    private int margin_left_right_dp = 0;
+    private int margin_top_bottom_dp = 0;
+    private OnDialogListener onDialogListener;
+    /**
+     * 时候播放动画
+     */
+    private boolean isPlayAnimate=true;
+
+    public WebViewDialog(@NonNull Context context) {
+        this(context, R.style.Red_Dialog);
+    }
+
+    public WebViewDialog(@NonNull Context context, @StyleRes int themeResId) {
+        super(context, themeResId);
+        this.mContext = context;
+        initPix();
+        init();
+    }
+
+    private void init() {
+//        View view = View.inflate(mContext, R.layout.dialog_webview, null);
+//        webView = (CommWebView) view.findViewById(R.id.cwv_view);
+        webView = new CommWebView(mContext);
+        webView.setTransparent(true);
+        webView.addJavascriptInterface(new JSCallNative(), "JsCallNative");
+        setContentView(webView);
+        setMargin();
+    }
+
+    public boolean isPlayAnimate() {
+        return isPlayAnimate;
+    }
+
+    public void setPlayAnimate(boolean playAnimate) {
+        isPlayAnimate = playAnimate;
+    }
+
+    /**
+     * 添加js与java互相调用类.
+     * <p>
+     * SuppressLint("JavascriptInterface") 表示webview的修复漏洞
+     *
+     * @param mapClazz js方法与java方法映射类
+     * @param objName  对象的名字
+     */
+    @SuppressLint("JavascriptInterface")
+    public WebViewDialog addJavascriptInterface(Object mapClazz, String objName) {
+        webView.addJavascriptInterface(mapClazz, objName);
+        return this;
+    }
+
+    /**
+     * 设置距离左边和右边的距离(单位：dp，默认30dp)
+     *
+     * @param margin_left_right_dp
+     */
+    public void setMargin_left_right_dp(int margin_left_right_dp) {
+        this.margin_left_right_dp = margin_left_right_dp;
+        setMargin();
+    }
+
+    /**
+     * 获取webview
+     *
+     * @return
+     */
+    public CommWebView getWebView() {
+        return webView;
+    }
+
+    /**
+     * 设置边距
+     *
+     * @param left_right
+     * @param top_bottom
+     */
+    public void setMargin(int left_right, int top_bottom) {
+        this.margin_left_right_dp = left_right;
+        this.margin_top_bottom_dp = top_bottom;
+        setMargin();
+    }
+
+    /**
+     * 设置距离顶部和底部的距离（单位：dp，默认65dp）
+     *
+     * @param margin_top_bottom_dp
+     */
+    public void setMargin_top_bottom_dp(int margin_top_bottom_dp) {
+        this.margin_top_bottom_dp = margin_top_bottom_dp;
+        setMargin();
+    }
+
+    private void setMargin() {
+        Window dialogWindow = getWindow();
+        WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+        dialogWindow.setGravity(Gravity.CENTER);
+        lp.width = screenWidth - 2 * dip2px(margin_left_right_dp);
+        lp.height = screenHeight - 2 * dip2px(margin_top_bottom_dp); // 高度
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        if (isPlayAnimate) {
+            startAnimate();
+        }
+    }
+
+    private void startAnimate() {
+        getWindow().setWindowAnimations(R.style.Red_Dialog);
+    }
+
+    /**
+     * 加载url
+     *
+     * @param url
+     */
+    public void loadUrl(String url) {
+        webView.setCurWebUrl(url);
+        webView.refresh();
+    }
+
+    public static int dip2px(float dipValue) {
+        final float scale = Resources.getSystem().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+    public void initPix() {
+        DisplayMetrics dm = mContext.getResources().getDisplayMetrics();
+        screenWidth = dm.widthPixels;
+        screenHeight = dm.heightPixels;
+    }
+
+    @Override
+    public void dismiss() {
+        if (webView != null) {
+            webView.onDestroy();
+        }
+        if (onDialogListener != null) {
+            onDialogListener.onClose();
+        }
+        super.dismiss();
+    }
+
+    public class JSCallNative {
+        @JavascriptInterface
+        public void closeDialog() {
+            if (mContext != null) {
+                /**
+                 * 4.4以上的webview，需要在子线程中调用js与java互相调用的代码
+                 */
+                ((Activity) mContext).runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        dismiss();
+                    }
+                });
+            }
+        }
+    }
+}
